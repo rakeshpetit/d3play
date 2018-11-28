592cc7086fc1d17987a0d62adcd2af31a72df5b6
Rakesh Arunachalam <raar@danskeit.co.in>
2018-11-28 17:20:23 +0530

    band scale dynamic widths

--- basics/bars.js
+++ basics/bars.js
@@ -1,19 +1,39 @@
-d3.json('basics/buildings.json').then((data) => {
-    console.log(data);
-    
-    var svg = d3.select('#chart-area').append('svg')
-    .attr('width', 1200)
+var svg = d3.select('#chart-area').append('svg')
+    .attr('width', 800)
     .attr('height', 800);
+
+d3.json('basics/buildings.json').then((data) => {
+    data.forEach(d => {
+        d.height = +d.height;
+    });
+
+    const arr = [];
+    data.map((i) => {
+        arr.push(i.name);
+    });
+
+    console.log(arr);
+
+    var x = d3.scaleBand()
+        .domain(arr)
+        .range([0, 600])
+        .paddingInner(0.3)
+        .paddingOuter(0.3);
+
+    var y = d3.scaleLinear()
+        .domain([0, 828])
+        .range([0, 400]);
+        
     var bars = svg.selectAll('rect').data(data);
 
     bars.enter().append('rect')
-        .attr('x', (d, i) => {
-            return (i * 120) + 300;
+        .attr('x', (d) => {
+            return x(d.name) + 40;
         })
         .attr('y', 200)
-        .attr('width', 70)
+        .attr('width', 40)
         .attr('height', (d) => {
-            return d.height * 2;
+            return y(d.height);
         })
         .attr('fill', 'gray');
 });
